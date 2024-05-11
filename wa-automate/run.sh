0af5dff03c4472b96f1282107ad68e39b65acb5f
Al3XKOoL <moonrotation@gmail.com>
2024-05-11 10:25:32 +0100

    Update run.sh

--- wa-automate/run.sh
+++ wa-automate/run.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
 # Escribe el contenido de la opción main_js en main.js
-echo "$MAIN_JS" > /share/wa-automate/main.js
+#echo "$MAIN_JS" > /share/wa-automate/main.js
 
 # Iniciar wa-automate
 node /share/wa-automate/main.js
