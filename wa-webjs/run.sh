8c3f0d82137e222277f6691152b97aaa48bfa9bc
Al3XKOoL <moonrotation@gmail.com>
2024-12-15 09:21:21 +0000

    Update run.sh

--- wa-webjs/run.sh
+++ wa-webjs/run.sh
@@ -1,32 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-#echo "Iniciando run.sh!"
-
 # Escribe el contenido de la opción main_js en main.js
 #echo "$MAIN_JS" > /share/wa-webjs/main.js
 
-#apk update && apk add build-base
-
-#apk add --no-cache python3 py3-pip
-#pip3 install flask flask-login flask-talisman --break-system-packages
-
-apt update
-#apt install -y software-properties-common
-#add-apt-repository ppa:xtradeb/apps -y
-apt install -y chromium
-
-#Move init here...
 cd /share/wa-webjs
-#npm init -y
-#npm install moment
-#npm install qrcode-terminal
-#npm install whatsapp-web.js
-#npm install puppeteer-core
-#npm install puppeteer
-#npm audit fix --force
-
 
 echo "iniciando main.js"
 # Iniciar wa-webjs y flask
-cd /share/wa-webjs && node main.js 
-#& python3 web.py
+cd /share/wa-webjs && node main.js & python3 web.py
